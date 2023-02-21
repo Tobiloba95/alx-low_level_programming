d704829c4bd51af1fb10a29b3e014aca778b6c9d
Tobiloba95 <oomolara40@gmail.com>
2023-02-21 04:00:37 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
+
+int main(void)
+{	
+	for (i = 0; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
