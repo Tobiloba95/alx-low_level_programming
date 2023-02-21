c1ffc5979e6f4f215de00ba93037f76d7d8f3014
Tobiloba95 <oomolara40@gmail.com>
2023-02-21 04:12:07 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include <main.h>
+#include "holberton.h"
 
 /**
  * main - Check description
@@ -9,10 +9,13 @@
 
 int main(void)
 {	
+	char word[9] = "Holberton";
 	int i;
 
 	for (i = 0; i < 9; i++)
+	{
 		_putchar(word[i]);
+	}
 	_putchar('\n');
 
 	return (0);
